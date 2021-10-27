92ca891a4a661eddb692a1c61b1393b5f81e68bf
1507lll <92914700+1507lll@users.noreply.github.com>
2021-10-27 13:18:23 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+     .rihgtimg{
+           float: right;
+           marging: 7px 0 7px 7px;
+     }   
+     
+     
+     
+     
+     
+     
+     
+</style>     
+<pre><h1 syle="text-align: center;">                                         my Portofolio:
+<pre><h2>Name:  Adinur</h2></pre>
+<pre><h2>Info: I live in Bishkek. 17 years old</h2></pre>
+<pre><h2>Phone number: 0558559989</h2></pre>
+
+
+<pre><h1;">
+</body>
+</html>
