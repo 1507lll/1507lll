d9cccac8e456d9d465f6f21736bf00695a4e5812
1507lll <92914700+1507lll@users.noreply.github.com>
2021-11-15 16:30:18 +0600

    Update adinur.php

--- adinur.php
+++ adinur.php
@@ -16,7 +16,7 @@
 <pre><h1 syle="text-align: center;">                                         my Portofolio:
 <pre><h2>Name:  Adilov </h2></pre>
 <pre><h2>Info: I live in Bishkek. 29 years old</h2></pre>                  
-<pre><h2>Phone number: 0558559989</h2></pre>
+<pre><h2>Phone number: 0558559988</h2></pre>
 <img src="index.jpg" alt="">
 
 <!DOCTYPE html>
